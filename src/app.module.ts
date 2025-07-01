c3c810e8b913e4fc1aa33d39b51e16cf1382a684
Mehdi <mahdizareiofficial@gmail.com>
2025-07-01 20:30:53 +0330

    feat: create AppModule with initial imports and configuration

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmConfig } from "./config/typeorm.config";
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
+import { SmsModule } from "./sms/sms.module";
+import { RedisModule } from "./redis/redis.module";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(TypeOrmConfig()),
+    AuthModule,
+    UserModule,
+    SmsModule,
+    RedisModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
