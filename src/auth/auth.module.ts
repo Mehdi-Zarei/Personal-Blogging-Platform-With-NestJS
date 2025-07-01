ce9b612ad195faf559dc13def2228b0a1d65f516
Mehdi <mahdizareiofficial@gmail.com>
2025-07-01 20:31:11 +0330

    feat: create AuthModule with necessary imports, controllers, and providers

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,15 @@
+import { Module } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserEntity } from "src/user/entities/user.entity";
+import { RedisService } from "src/redis/redis.service";
+import { SmsService } from "src/sms/sms.service";
+import { HttpModule } from "@nestjs/axios";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity]), HttpModule],
+  controllers: [AuthController],
+  providers: [AuthService, RedisService, SmsService],
+})
+export class AuthModule {}
