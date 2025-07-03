d7027abd6c4fd23f0b7e4e5e38752d45b32c6df2
Mehdi <mahdizareiofficial@gmail.com>
2025-07-03 18:30:29 +0330

    feat: enhance UserController with role-based access and API documentation improvements

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,34 +1,54 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Controller, Body, Get, UseGuards, Query, DefaultValuePipe, ParseIntPipe, Param, Patch, Req } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { AuthGuard } from "src/common/guards/auth.guard";
+import { Roles } from "src/common/decorators/roles.decorator";
+import { ApiBearerAuth, ApiConsumes, ApiOperation, ApiParam, ApiQuery, ApiTags } from "@nestjs/swagger";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { Request } from "express";
+import { SwaggerConsumes } from "src/common/enums/swaggerConsumes.enum";
 
-@Controller('user')
+@ApiTags("User")
+@Controller("user")
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  @UseGuards(AuthGuard)
+  @Roles("ADMIN")
+  @ApiBearerAuth("accessToken")
+  @ApiOperation({ summary: "Admin can get all users with pagination(without password)." })
+  @ApiQuery({ name: "page", required: false, description: "صفحه مورد نظر" })
+  @ApiQuery({ name: "limit", required: false, description: "تعداد آیتم‌ها در هر صفحه" })
+  @Get("/")
+  getAll(@Query("page", new DefaultValuePipe(1), ParseIntPipe) page: number, @Query("limit", new DefaultValuePipe(10), ParseIntPipe) limit: number) {
+    return this.userService.getAll(page, limit);
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
+  @UseGuards(AuthGuard)
+  @Roles("ADMIN")
+  @ApiBearerAuth("accessToken")
+  @ApiOperation({ summary: "Admin can get main user." })
+  @Get(":id")
+  getOne(@Param("id", ParseIntPipe) id: number) {
+    return this.userService.getOne(id);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  @UseGuards(AuthGuard)
+  @Roles("ADMIN")
+  @ApiBearerAuth("accessToken")
+  @Patch(":id")
+  @ApiOperation({ summary: "Admin can ban or lifting the ban users." })
+  toggleBanStatus(@Param("id", ParseIntPipe) id: number) {
+    return this.userService.toggleBanStatus(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth("accessToken")
+  @ApiConsumes(SwaggerConsumes.JSON)
+  @ApiOperation({ summary: "User Can Update Their Profile." })
+  @Patch()
+  updateProfile(@Req() req: Request, @Body() updateUserDto: UpdateUserDto) {
+    const userId = req.user!.id;
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+    return this.userService.updateProfile(userId, updateUserDto);
   }
 }
