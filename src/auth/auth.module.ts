cfaa25b2624977f959530a15dff08e9e716f7143
Mehdi <mahdizareiofficial@gmail.com>
2025-07-03 20:52:14 +0330

    feat: add JwtModule to AuthModule imports and include AuthGuard in providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,13 +6,15 @@ import { UserEntity } from "src/user/entities/user.entity";
 import { RedisService } from "src/redis/redis.service";
 import { SmsService } from "src/sms/sms.service";
 import { HttpModule } from "@nestjs/axios";
-import { JwtService } from "@nestjs/jwt";
+import { JwtModule, JwtService } from "@nestjs/jwt";
 import { TokenService } from "./token.service";
 import { HashService } from "./dto/bcrypt.service";
+import { AuthGuard } from "src/common/guards/auth.guard";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity]), HttpModule],
+  imports: [TypeOrmModule.forFeature([UserEntity]), HttpModule, JwtModule],
   controllers: [AuthController],
-  providers: [AuthService, RedisService, SmsService, JwtService, TokenService, HashService],
+  providers: [AuthService, RedisService, SmsService, JwtService, TokenService, HashService, AuthGuard],
+  exports: [AuthGuard, JwtModule, TypeOrmModule],
 })
 export class AuthModule {}
