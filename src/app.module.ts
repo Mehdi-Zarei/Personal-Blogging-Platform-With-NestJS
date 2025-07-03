95bf94db68074cf3550f0a85a1fc781eb69b247e
Mehdi <mahdizareiofficial@gmail.com>
2025-07-03 20:52:09 +0330

    fix: update imports and formatting in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,13 @@ import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
 import { SmsModule } from "./sms/sms.module";
 import { RedisModule } from "./redis/redis.module";
-import { CategoryModule } from './category/category.module';
+import { CategoryModule } from "./category/category.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(TypeOrmConfig()),
-    AuthModule,
-    UserModule,
-    SmsModule,
-    RedisModule,
-    CategoryModule,
-  ],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig()), AuthModule, UserModule, SmsModule, RedisModule, CategoryModule, JwtModule],
   controllers: [],
   providers: [],
+  exports: [JwtModule],
 })
 export class AppModule {}
