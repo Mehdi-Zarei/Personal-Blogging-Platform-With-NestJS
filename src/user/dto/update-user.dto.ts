33c989dbe170f1617e34c6560f5d600bf8f3572b
Mehdi <mahdizareiofficial@gmail.com>
2025-07-03 18:30:45 +0330

    feat: redefine UpdateUserDto with detailed properties for user updates

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,48 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-user.dto';
+import { ApiPropertyOptional } from "@nestjs/swagger";
+import { IsEmail, IsEnum, IsOptional, IsString, Length } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @Length(3, 20, { message: "نام شما باید حداقل 3 و حداکثر 20 کاراکتر طول داشته باشد." })
+  @IsString({ message: "نام شما باید به حروف باشد." })
+  @IsOptional()
+  @ApiPropertyOptional({ example: "Mehdi", description: "نام کاربر" })
+  name?: string;
+
+  @IsEmail({}, { message: "لطفا یک ایمیل معتبر وارد کنید." })
+  @IsOptional()
+  @ApiPropertyOptional({ example: "user@example.com", description: "ایمیل معتبر کاربر" })
+  email?: string;
+
+  @IsOptional()
+  @Length(8, 50, { message: "رمز عبور بایستی حداقل 8 حرف داشته باشد." })
+  @ApiPropertyOptional({ example: "mypassword123", description: "رمز عبور امن" })
+  password?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiPropertyOptional({
+    example: "profile.jpg",
+    description: "آدرس تصویر پروفایل",
+    required: false,
+  })
+  profileImage?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(10, 200, { message: "بیوگرافی باید حداقل 10 و حداکثر 200 کاراکتر باشد." })
+  @ApiPropertyOptional({
+    example: "من یک توسعه‌دهنده هستم.",
+    description: "توضیحی در مورد کاربر",
+    required: false,
+  })
+  bio?: string;
+
+  @ApiPropertyOptional({
+    example: "Male",
+    description: "جنسیت کاربر",
+    enum: ["Male", "Female"],
+  })
+  @IsEnum(["Male", "Female"], { message: "جنسیت باید مرد یا زن باشد." })
+  @IsOptional()
+  gender?: "Male" | "Female";
+}
