f0e5023c77774e4c76b0c6bd3e639e05a133fbf9
Mehdi <mahdizareiofficial@gmail.com>
2025-07-04 21:37:41 +0330

    feat: add ArticleModule to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { SmsModule } from "./sms/sms.module";
 import { RedisModule } from "./redis/redis.module";
 import { CategoryModule } from "./category/category.module";
 import { JwtModule } from "@nestjs/jwt";
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig()), AuthModule, UserModule, SmsModule, RedisModule, CategoryModule, JwtModule],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig()), AuthModule, UserModule, SmsModule, RedisModule, CategoryModule, JwtModule, ArticleModule],
   controllers: [],
   providers: [],
   exports: [JwtModule],
