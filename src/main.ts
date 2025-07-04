0d0a5795d63ac73ae3fcd3fb9aa2a79864053cd8
Mehdi <mahdizareiofficial@gmail.com>
2025-07-04 20:06:55 +0330

    feat: enhance main.ts to use NestExpressApplication and serve static assets

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ import { AppModule } from "./app.module";
 import { swaggerConfigInit } from "./config/swagger.config";
 import * as cookieParser from "cookie-parser";
 import { ValidationPipe } from "@nestjs/common";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser(process.env.COOKIE_SECRET)); //todo
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useStaticAssets(join(__dirname, "..", "public"));
   swaggerConfigInit(app);
 
   const { PORT } = process.env;
