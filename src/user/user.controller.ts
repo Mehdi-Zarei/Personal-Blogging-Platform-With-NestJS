549ca900f9b8cb3b4afe09ff5cd1803b6ace4815
Mehdi <mahdizareiofficial@gmail.com>
2025-07-04 20:07:27 +0330

    feat: add uploadAvatar endpoint for user profile image upload

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Body, Get, UseGuards, Query, DefaultValuePipe, ParseIntPipe, Param, Patch, Req } from "@nestjs/common";
+import { Controller, Body, Get, UseGuards, Query, DefaultValuePipe, ParseIntPipe, Param, Patch, Req, Post, UseInterceptors, UploadedFile, HttpCode } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { AuthGuard } from "src/common/guards/auth.guard";
 import { Roles } from "src/common/decorators/roles.decorator";
-import { ApiBearerAuth, ApiConsumes, ApiOperation, ApiParam, ApiQuery, ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOperation, ApiParam, ApiQuery, ApiTags } from "@nestjs/swagger";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { Request } from "express";
 import { SwaggerConsumes } from "src/common/enums/swaggerConsumes.enum";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { multerConfig } from "src/upload/multer.config";
+import { UploadAvatarDto } from "./dto/upload-avatar.dto";
 
 @ApiTags("User")
 @Controller("user")
@@ -43,12 +46,26 @@ export class UserController {
 
   @UseGuards(AuthGuard)
   @ApiBearerAuth("accessToken")
-  @ApiConsumes(SwaggerConsumes.JSON, SwaggerConsumes.FORM)
+  @ApiConsumes(SwaggerConsumes.FORM)
   @ApiOperation({ summary: "User Can Update Their Profile." })
   @Patch()
   updateProfile(@Req() req: Request, @Body() updateUserDto: UpdateUserDto) {
     const userId = req.user!.id;
 
     return this.userService.updateProfile(userId, updateUserDto);
+  }
+
+  @ApiOperation({ summary: "Upload profile avatar." })
+  @ApiBearerAuth("accessToken")
+  @ApiConsumes(SwaggerConsumes.MULTIPART)
+  @ApiBody({ type: UploadAvatarDto })
+  @HttpCode(200)
+  @UseGuards(AuthGuard)
+  @UseInterceptors(FileInterceptor("profileImage", multerConfig("public/images/users-profile/", 2, [".jpg", ".jpeg", ".png"])))
+  @Post("upload-avatar")
+  uploadAvatar(@Req() req: Request, @UploadedFile() file: Express.Multer.File) {
+    const userId = req.user!.id;
+
+    return this.userService.uploadAvatar(userId, file);
   }
 }
