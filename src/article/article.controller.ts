b81d4e2583fb1ee03ee209f76f17c528261c2112
Mehdi <mahdizareiofficial@gmail.com>
2025-07-08 19:40:23 +0330

    feat: enhance ArticleController with user authentication and additional article operations

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,34 +1,89 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ArticleService } from './article.service';
-import { CreateArticleDto } from './dto/create-article.dto';
-import { UpdateArticleDto } from './dto/update-article.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, Query, ParseIntPipe, Put, UseInterceptors, UploadedFiles } from "@nestjs/common";
+import { ArticleService } from "./article.service";
+import { CreateArticleDto } from "./dto/create-article.dto";
+import { UpdateArticleDto } from "./dto/update-article.dto";
+import { ApiBearerAuth, ApiConsumes, ApiOperation, ApiQuery, ApiTags } from "@nestjs/swagger";
+import { AuthGuard } from "src/common/guards/auth.guard";
+import { SwaggerConsumes } from "src/common/enums/swaggerConsumes.enum";
+import { Request } from "express";
+import { PaginationDto } from "src/common/dto/pagination.dto";
+import { FilesInterceptor } from "@nestjs/platform-express";
+import { multerConfig } from "src/upload/multer.config";
 
-@Controller('article')
+@ApiTags("Article")
+@Controller("article")
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
-  @Post()
-  create(@Body() createArticleDto: CreateArticleDto) {
-    return this.articleService.create(createArticleDto);
+  @UseGuards(AuthGuard)
+  @ApiConsumes(SwaggerConsumes.MULTIPART)
+  @ApiOperation({ summary: "Create Article" })
+  @ApiBearerAuth("accessToken")
+  @UseInterceptors(FilesInterceptor("image", 10, multerConfig("public/images/article/", 2, [".jpg", ".jpeg", ".png"])))
+  @Post("/")
+  create(@Body() createArticleDto: CreateArticleDto, @Req() req: Request, @UploadedFiles() file: Express.Multer.File[]) {
+    const userId = req.user!.id;
+    return this.articleService.create(createArticleDto, userId, file);
   }
 
-  @Get()
-  findAll() {
-    return this.articleService.findAll();
+  @ApiOperation({ summary: "Get All Blogs." })
+  @ApiQuery({ name: "page", required: false })
+  @ApiQuery({ name: "limit", required: false })
+  @ApiQuery({ name: "search", required: false })
+  @Get("/")
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.articleService.findAll(paginationDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.articleService.findOne(+id);
+  @ApiBearerAuth("accessToken")
+  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: "User Can Get Their Articles." })
+  @Get("/my-blogs")
+  myBlogs(@Req() req: Request) {
+    const userId = req.user!.id;
+    return this.articleService.myBlogs(userId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
-    return this.articleService.update(+id, updateArticleDto);
+  @ApiBearerAuth("accessToken")
+  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: "User Can Update Their Articles." })
+  @ApiConsumes(SwaggerConsumes.FORM)
+  @Patch(":id")
+  update(@Param("id", ParseIntPipe) id: number, @Body() updateArticleDto: UpdateArticleDto, @Req() req: Request) {
+    const userId = req.user!.id;
+    return this.articleService.update(id, updateArticleDto, userId);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.articleService.remove(+id);
+  @ApiBearerAuth("accessToken")
+  @UseGuards(AuthGuard)
+  @ApiOperation({ summary: "User Can Remove Their Articles." })
+  @Delete(":id")
+  remove(@Param("id", ParseIntPipe) id: number, @Req() req: Request) {
+    const userId = req.user!.id;
+    return this.articleService.remove(id, userId);
+  }
+
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth("accessToken")
+  @ApiOperation({ summary: "User Can Like/Unlike Articles." })
+  @Put("like/:id")
+  likeToggle(@Param("id", ParseIntPipe) id: number, @Req() req: Request) {
+    const userId = req.user!.id;
+    return this.articleService.likeToggle(id, userId);
+  }
+
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth("accessToken")
+  @ApiOperation({ summary: "User Can Save/Unsave Articles." })
+  @Put("bookmark/:id")
+  bookmarkToggle(@Param("id", ParseIntPipe) id: number, @Req() req: Request) {
+    const userId = req.user!.id;
+    return this.articleService.bookmarkToggle(id, userId);
+  }
+
+  @ApiOperation({ summary: "Get One Article." })
+  @Get(":id")
+  getOne(@Param("id", ParseIntPipe) id: number) {
+    return this.articleService.getOne(id);
   }
 }
