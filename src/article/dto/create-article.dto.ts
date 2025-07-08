ef62ed375dd6d309dff397121a6a0fbcbfdd11a6
Mehdi <mahdizareiofficial@gmail.com>
2025-07-08 19:40:33 +0330

    feat: implement CreateArticleDto with validation and Swagger documentation

--- src/article/dto/create-article.dto.ts
+++ src/article/dto/create-article.dto.ts
@@ -1,1 +1,33 @@
-export class CreateArticleDto {}
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumberString, IsString, Length } from "class-validator";
+
+export class CreateArticleDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @Length(10, 100)
+  title: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @Length(10, 300)
+  description: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @Length(100)
+  content: string;
+
+  @ApiProperty()
+  @IsNumberString()
+  @IsNotEmpty()
+  timeForStudy: string;
+
+  @ApiPropertyOptional({ format: "binary", isArray: true })
+  images: string[];
+
+  @ApiProperty({ enum: ["Publish", "Draft", "Unpublish"] })
+  status: string;
+}
