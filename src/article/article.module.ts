cb2e1816d316f0cec8dbd6963b19ccc278424060
Mehdi <mahdizareiofficial@gmail.com>
2025-07-08 19:40:27 +0330

    refactor: organize imports and enhance ArticleModule with TypeOrmModule for entities

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -1,8 +1,15 @@
-import { Module } from '@nestjs/common';
-import { ArticleService } from './article.service';
-import { ArticleController } from './article.controller';
+import { Module } from "@nestjs/common";
+import { ArticleService } from "./article.service";
+import { ArticleController } from "./article.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ArticleEntity } from "./entities/article.entity";
+import { JwtModule, JwtService } from "@nestjs/jwt";
+import { AuthModule } from "src/auth/auth.module";
+import { LikeEntity } from "./entities/like.entity";
+import { BookmarkEntity } from "./entities/bookmark.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ArticleEntity, LikeEntity, BookmarkEntity]), AuthModule],
   controllers: [ArticleController],
   providers: [ArticleService],
 })
